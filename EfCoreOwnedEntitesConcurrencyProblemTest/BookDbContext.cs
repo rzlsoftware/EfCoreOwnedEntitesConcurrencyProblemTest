c5e918a66c272187c036359934dff7094c72d1c6
Jakob Wegenschimmel <jwegenschimmel@rzl.at>
2018-08-02 13:18:29 +0200

    Add DbContext

---
+++ EfCoreOwnedEntitesConcurrencyProblemTest/BookDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreOwnedEntitesConcurrencyProblemTest
+{
+    public class BookDbContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer(@"Server=.;Database=ReproEfCoreOwnedEntitesConcurrencyProblemTest;Trusted_Connection=True;MultipleActiveResultSets=True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>(b =>
+                {
+                    b.OwnsOne(a => a.Name);
+                    b.Property(p => p.Rowversion).IsRowVersion();
+                });
+        }
+    }
+}
