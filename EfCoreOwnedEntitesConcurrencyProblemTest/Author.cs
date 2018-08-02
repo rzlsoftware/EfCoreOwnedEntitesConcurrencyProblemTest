2703f8f236caf5ce721ea95da17e0cfc0e547e56
Jakob Wegenschimmel <jwegenschimmel@rzl.at>
2018-08-02 13:14:31 +0200

    Add Model

---
+++ EfCoreOwnedEntitesConcurrencyProblemTest/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace EfCoreOwnedEntitesConcurrencyProblemTest
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public Name Name { get; set; }
+        public byte[] Rowversion { get; set; }
+    }
+}
